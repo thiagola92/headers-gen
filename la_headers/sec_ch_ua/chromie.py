d8c69fe551d963d20b79e989098b18495c3aea11
thiagola92 <thiagola92@gmail.com>
2022-09-10 08:23:16 -0300

    Update sec-ch-ua from Chrome

--- la_headers/sec_ch_ua/chromie.py
+++ la_headers/sec_ch_ua/chromie.py
@@ -1,5 +1,6 @@
 # fmt: off
 CHROME_SEC_CH_UA = (
+    ("105", '"Google Chrome";v="{major}", "Not)A;Brand";v="8", "Chromium";v="{major}"'),
     ("99", '" Not;A Brand";v="99", "Chromium";v="{major}", "Google Chrome";v="{major}"'),
     ("0", '" Not;A Brand";v="99", "Google Chrome";v="{major}", "Chromium";v="{major}"'),
 )
